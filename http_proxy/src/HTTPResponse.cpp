c912f7b559c0ccd7dd204bce6054a0c1a87427b6
Janus Chen <janus.chen@duke.edu>
2023-02-22 16:16:38 -0500

    2.23

--- http_proxy/src/HTTPResponse.cpp
+++ http_proxy/src/HTTPResponse.cpp
@@ -1,22 +1,65 @@
-#include <string>
 #include "HTTPResponse.h"
-#include "Socket.h"
+#include "boost/beast.hpp"
+#include "TimeParser.h"
+#include <string>
+#include <string_view>
 
-void HTTPResponse::parseStartLine() {
-    size_t URIPos = startLine.find(" ");
-    URI = startLine.substr(0, URIPos);
-    std::string temp = startLine.substr(URIPos + 1); 
-    size_t statusCodePos = temp.find(" ");
-    statusCode = temp.substr(0, statusCodePos); 
-    temp = temp.substr(statusCodePos + 1); 
-    size_t statusMsgPos = temp.find("\r\n");
-    statusMsg = temp.substr(0, statusMsgPos);
+namespace http = boost::beast::http;
+
+void HTTPResponse::cacheability() {
+    auto it = response.base().find(http::field::cache_control);
+    if (it != response.base().end()) {
+        std::string_view cache_control = it->value();
+        if (cache_control.find("private") != std::string::npos) {
+            uncacheable_reason = "Cache-Control: private";
+            return;
+        }
+        if (cache_control.find("no-store") != std::string::npos) {
+            uncacheable_reason = "Cache-Control: no-store";
+            return;
+        }
+        if (cache_control.find("max-age=0") != std::string::npos ||
+            cache_control.find("no-cache") != std::string::npos ||
+            cache_control.find("must-revalidate") != std::string::npos) {
+            require_validation = true;
+        }
+        if (cache_control.find("max-age=") != std::string::npos &&
+            cache_control.find("max-age=0") == std::string::npos) {
+            expires = true;
+        }
+    } else {
+        uncacheable_reason = "No Cache-Control header is found!";
+        return;
+    }
+    it = response.base().find(http::field::expires);
+    if (it != response.base().end()) {
+        expires = true;
+    }
+    it = response.base().find(http::field::etag);
+    if (it != response.base().end()) {
+        can_validate = true;
+    }
+    it = response.base().find(http::field::last_modified);
+    if (it != response.base().end()) {
+        can_validate = true;
+    }
+    if (require_validation && !can_validate) {
+        uncacheable_reason = "Requires validation but no etag and last-modified is found!";
+        return;
+    }
+    if (!expires && !require_validation) {
+        uncacheable_reason = "This response will neither expire nor require re-validation!";
+        return;
+    }
+    if (expires) {
+        set_expire_time();
+    }
+    cacheable = true;
 }
 
-void HTTPResponse::checkCache(std::string ID) {
-    std::string cacheControl = getFromHeader("Cache-Control");
-    std::string ETag = getFromHeader("ETag");
-    std::string lastModified = getFromHeader("Last-Modified");
+void HTTPResponse::set_expire_time() {
+    auto it = response.base().find(http::field::cache_control);
+    if (it != response.base().end()) {
 
-    
+    }
 }
--- http_proxy/src/HTTPResponse.h
+++ http_proxy/src/HTTPResponse.h
@@ -1,27 +1,40 @@
-#ifndef _HTTP_RESPONSE_H_
-#define _HTTP_RESPONSE_H_
+#ifndef _VALIDATOR_H_
+#define _VALIDATOR_H_
 
 #include <string>
-#include <map>
-#include "boost/asio.hpp"
 #include "boost/beast.hpp"
 
 namespace http = boost::beast::http;
 
 class HTTPResponse {
 private:
+    http::response<http::dynamic_body> response;
 
-    
+    // The response is cacheable.
+    bool cacheable;
+    // The response need to be validated every time.
+    bool require_validation;
+    // The response has a e-tag or last-modified field.
+    bool can_validate;
+    // The response has a expiring time.
+    bool expires;
+    // The expire time.
+    std::time_t expire_time;
+    // The reason that cannot be cached.
+    std::string uncacheable_reason;
+
+    void cacheability();
+
+    void set_expire_time();
 
 public:
 
-    HTTPResponse(std::string _content): HTTPMessage(_content) {}
+    HTTPResponse(http::response<http::dynamic_body> & _res): 
+        response(_res), cacheable(false), require_validation(false), can_validate(false), expires(false) { cacheability(); }
 
-    const std::string & getURI() const { return URI; }
-    const std::string & getstatusCode() const { return statusCode; }
-    const std::string & getstatusMsg() const { return statusMsg; }
-
-    void checkCache(std::string ID);
+    HTTPResponse(http::response<http::dynamic_body> && _res): 
+        response(_res), cacheable(false), require_validation(false), can_validate(false), expires(false) { cacheability(); }
+    
 };
 
 #endif
