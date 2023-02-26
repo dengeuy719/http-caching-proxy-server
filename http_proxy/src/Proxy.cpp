506659018c5be0a3201c2dbdd44971626a6a1f2c
yd171 <yd171@duke.edu>
2023-02-25 21:31:46 -0500

    connect 2.25

--- http_proxy/src/Proxy.cpp
+++ http_proxy/src/Proxy.cpp
@@ -76,221 +76,164 @@ void handle_request(boost::asio::ip::tcp::socket & socket) {
         Log::getInstance().write(req.getID() + ": ERROR Bad response. Reason: " + std::string(e.what()));
     }
 }
+// //********  boost version connect ******
+// void handle_CONNECT(HTTPRequest& req) {
+//   // Connect to the remote server
+//   boost::asio::ip::tcp::socket& client_socket = req.getClientSocket();
+//   boost::asio::ip::tcp::socket& server_socket = *req.getServerSocket();
 
+//   // Send a success response to the client
+//   http::response<http::dynamic_body> new_response;
+//   new_response.version(11);
+//   new_response.result(http::status::ok);
+//   new_response.prepare_payload();
+//   req.sendBack(new_response);
 
-void handle_CONNECT(HTTPRequest& req) {
-  // Connect to the remote server
-  boost::asio::ip::tcp::socket& client_socket = req.getClientSocket();
-  boost::asio::ip::tcp::socket& server_socket = *req.getServerSocket();
+//   // Set both sockets to non-blocking mode
+//   client_socket.non_blocking(true);
+//   server_socket.non_blocking(true);
 
-  // Send a success response to the client
-  http::response<http::dynamic_body> new_response;
-  new_response.version(11);
-  new_response.result(http::status::ok);
-  new_response.prepare_payload();
-  req.sendBack(new_response);
+//   bool client_open = true;
+//   bool server_open = true;
+//   while (client_open || server_open) {
+//     // Check if there is data available to read from either socket
+//     boost::asio::ip::tcp::socket* readable_socket = nullptr;
+//     if (client_open) {
+//       if (client_socket.available()) {
+//         readable_socket = &client_socket;
+//       }
+//     }
+//     if (server_open) {
+//       if (server_socket.available()) {
+//         readable_socket = &server_socket;
+//       }
+//     }
 
-  // Set both sockets to non-blocking mode
-  client_socket.non_blocking(true);
-  server_socket.non_blocking(true);
+//     if (readable_socket) {
+//       // Read data from the socket in a loop until all available data has been read
+//       std::vector<char> buffer(65536);
+//       while (true) {
+//         boost::system::error_code error;
+//         size_t bytesRead = readable_socket->read_some(boost::asio::buffer(buffer.data(), buffer.size()), error);
+//         std::cout << "bytesRead: "<< bytesRead << std::endl;
+//         if (error) {
+//           if (error == boost::asio::error::eof) {
+//             if (readable_socket == &client_socket) {
+//               client_open = false;
+//               break;
+//             } else {
+//               server_open = false;
+//               break;
+//             }
+//           } else if (error != boost::asio::error::would_block) {
+//             std::cerr << "ERROR receiving data: " << error.message() << std::endl;
+//             break;
+//           }
+//         } else {
+//           // Write the data to the other socket
+//           size_t bytesToSend = bytesRead;
+//           while (bytesToSend > 0) {
+//             size_t bytes_sent = boost::asio::write(readable_socket == &client_socket ? server_socket : client_socket, 
+//               boost::asio::buffer(buffer.data(), bytesToSend), error);
+//             if (error && error != boost::asio::error::would_block) {
+//               std::cerr << "ERROR sending data: " << error.message() << std::endl;
+//               std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//               continue;
+//             }
+//             bytesToSend -= bytes_sent;
+//           }
+//           if (error && error != boost::asio::error::would_block) {
+//             std::cerr << "ERROR sending data: " << error.message() << std::endl;
+//             std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//             continue;
+//           }
+//           // Check if all data has been read from the socket
+//           if (bytesRead < buffer.size()) {
+//             break;
+//           }
+//         }
+//       }
+//     }
+//   }
+//   // Close sockets
+//   boost::system::error_code error;
+//   client_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
+//   client_socket.close();
+//   server_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
+//   server_socket.close();
+//     std::stringstream str;
+//     str << req.getID() << ": Tunnel closed"<< std::endl;
+//     std::cout << str.str() << std::endl;
+// }
 
-  bool client_open = true;
-  bool server_open = true;
-  while (client_open || server_open) {
-    // Check if there is data available to read from either socket
-    boost::asio::ip::tcp::socket* readable_socket = nullptr;
-    if (client_open) {
-      if (client_socket.available()) {
-        readable_socket = &client_socket;
-      }
-    }
-    if (server_open) {
-      if (server_socket.available()) {
-        readable_socket = &server_socket;
-      }
-    }
+void handle_CONNECT(HTTPRequest & req) {
+    // Connect to the remote server
+    int server_sockfd = req.getServerSocket()->native_handle();
+    int client_sockfd = req.getClientSocket().native_handle();
+    // Send a success response to the client
+    http::response<http::dynamic_body> new_response;
+    new_response.version(11);
+    new_response.result(http::status::ok);
+    new_response.prepare_payload();
+    req.sendBack(new_response);
 
-    if (readable_socket) {
-      // Read data from the socket in a loop until all available data has been read
-      std::vector<char> buffer(4096);
-      while (true) {
-        boost::system::error_code error;
-        size_t bytesRead = readable_socket->read_some(boost::asio::buffer(buffer.data(), buffer.size()), error);
-        std::cout << "n: "<< bytesRead << std::endl;
-        if (error) {
-          if (error == boost::asio::error::eof) {
-            if (readable_socket == &client_socket) {
-              client_open = false;
-            } else {
-              server_open = false;
-            }
-          } else if (error != boost::asio::error::would_block) {
-            std::cerr << "ERROR receiving data: " << error.message() << std::endl;
+    // // Set both sockets to non-blocking mode
+    // int flags = fcntl(client_sockfd, F_GETFL, 0);
+    // fcntl(client_sockfd, F_SETFL, flags | O_NONBLOCK);
+    // flags = fcntl(server_sockfd, F_GETFL, 0);
+    // fcntl(server_sockfd, F_SETFL, flags | O_NONBLOCK);
+
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(client_sockfd, &readfds);
+    FD_SET(server_sockfd, &readfds);
+    int max_fd = std::max(client_sockfd, server_sockfd) + 1;
+    std::vector<char> buffer(65536);
+    while (true) {
+        fd_set temp_set = readfds;
+        if (select(max_fd, &temp_set, NULL, NULL, NULL) < 0) {
+            perror("ERROR in select");
             break;
-          }
-        } else {
-          // Write the data to the other socket
-          size_t bytesToSend = bytesRead;
-          while (bytesToSend > 0) {
-            size_t bytes_sent = boost::asio::write(readable_socket == &client_socket ? server_socket : client_socket, 
-              boost::asio::buffer(buffer.data(), bytesToSend), error);
-            if (error && error != boost::asio::error::would_block) {
-              std::cerr << "ERROR sending data: " << error.message() << std::endl;
-              std::this_thread::sleep_for(std::chrono::milliseconds(100));
-              continue;
-            }
-            bytesToSend -= bytes_sent;
-          }
-          if (error && error != boost::asio::error::would_block) {
-            std::cerr << "ERROR sending data: " << error.message() << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            continue;
-          }
-          // Check if all data has been read from the socket
-          if (bytesRead < buffer.size()) {
-            break;
-          }
         }
-      }
+        if (FD_ISSET(client_sockfd, &temp_set)) {
+            int n = recv(client_sockfd, &buffer.data()[0], sizeof(buffer), 0);
+            std::cout << "client: "<<n << std::endl;
+            if (n < 0) {
+                if (errno != EWOULDBLOCK) {
+                    perror("ERROR receiving data from client");
+                    break;
+                }
+            } else if (n == 0) {
+                break;
+            } else {
+                if (send(server_sockfd, &buffer.data()[0], n, 0) < 0) {
+                    perror("ERROR sending data to server");
+                    break;
+                }
+            }
+        }
+        if (FD_ISSET(server_sockfd, &temp_set)) {
+            int n = recv(server_sockfd, &buffer.data()[0], sizeof(buffer), 0);
+            std::cout << "server: "<<n << std::endl;
+            if (n < 0) {
+                if (errno != EWOULDBLOCK) {
+                    perror("ERROR receiving data from server");
+                    break;
+                }
+            } else if (n == 0) {
+                break;
+            } else {
+                if (send(client_sockfd, &buffer.data()[0], n, 0) < 0) {
+                    perror("ERROR sending data to client");
+                    break;
+                }
+            }
+        }
     }
-  }
-  // Close sockets
-  boost::system::error_code error;
-  client_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
-  client_socket.close();
-  server_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
-  server_socket.close();
-    std::stringstream str;
-    str << req.getID() << ": Tunnel closed"<< std::endl;
-    std::cout << str.str() << std::endl;
+    // Close sockets
+    close(server_sockfd);
+    close(client_sockfd);
 }
-
-// void handle_CONNECT(HTTPRequest & req) {
-//     // Connect to the remote server
-//     int server_sockfd = req.getServerSocket()->native_handle();
-//     int client_sockfd = req.getClientSocket().native_handle();
-//     // Send a success response to the client
-//     http::response<http::dynamic_body> new_response;
-//     new_response.version(11);
-//     new_response.result(http::status::ok);
-//     new_response.prepare_payload();
-//     req.sendBack(new_response);
-
-//     //version1: works, but slow
-//     // Set both sockets to non-blocking mode
-// int flags = fcntl(client_sockfd, F_GETFL, 0);
-// fcntl(client_sockfd, F_SETFL, flags | O_NONBLOCK);
-// flags = fcntl(server_sockfd, F_GETFL, 0);
-// fcntl(server_sockfd, F_SETFL, flags | O_NONBLOCK);
-
-// fd_set readfds;
-// FD_ZERO(&readfds);
-// FD_SET(client_sockfd, &readfds);
-// FD_SET(server_sockfd, &readfds);
-// int max_fd = std::max(client_sockfd, server_sockfd) + 1;
-// char buffer[65536] = {0};
-// while (true) {
-//     fd_set temp_set = readfds;
-//     if (select(max_fd, &temp_set, NULL, NULL, NULL) < 0) {
-//         perror("ERROR in select");
-//         break;
-//     }
-//     if (FD_ISSET(client_sockfd, &temp_set)) {
-//         int n = recv(client_sockfd, buffer, sizeof(buffer), 0);
-//         std::cout << "client: "<<n << std::endl;
-//         if (n < 0) {
-//             if (errno != EWOULDBLOCK) {
-//                 perror("ERROR receiving data from client");
-//                 break;
-//             }
-//         } else if (n == 0) {
-//             break;
-//         } else {
-//             if (send(server_sockfd, buffer, n, 0) < 0) {
-//                 perror("ERROR sending data to server");
-//                 break;
-//             }
-//         }
-//     }
-//     if (FD_ISSET(server_sockfd, &temp_set)) {
-//         int n = recv(server_sockfd, buffer, sizeof(buffer), 0);
-//         std::cout << "server: "<<n << std::endl;
-//         if (n < 0) {
-//             if (errno != EWOULDBLOCK) {
-//                 perror("ERROR receiving data from server");
-//                 break;
-//             }
-//         } else if (n == 0) {
-//             break;
-//         } else {
-//             if (send(client_sockfd, buffer, n, 0) < 0) {
-//                 perror("ERROR sending data to client");
-//                 break;
-//             }
-//         }
-//     }
-// }
-// // Close sockets
-// close(server_sockfd);
-// close(client_sockfd);
-// }
-
-//     // int flags = fcntl(client_sockfd, F_GETFL, 0);
-//     // fcntl(client_sockfd, F_SETFL, flags | O_NONBLOCK);
-//     // flags = fcntl(server_sockfd, F_GETFL, 0);
-//     // fcntl(server_sockfd, F_SETFL, flags | O_NONBLOCK);
-//     // fd_set readfds;
-//     // FD_ZERO(&readfds);
-//     // FD_SET(client_sockfd, &readfds);
-//     // FD_SET(server_sockfd, &readfds);
-//     // int max_fd = std::max(client_sockfd, server_sockfd) + 1;
-//     // while (true) {
-//     //     fd_set temp_set = readfds;
-//     //     //std::cout << "start select"<< std::endl;
-//     //     if (select(max_fd, &temp_set, NULL, NULL, NULL) < 0) {
-//     //         perror("ERROR in select");
-//     //         break;
-//     //     }
-//     //     //std::cout << "finish select"<< std::endl;
-//     //     if (FD_ISSET(client_sockfd, &temp_set)) {
-//     //         char buffer[65536];
-//     //         //std::cout << "inside client_sockfd"<< std::endl;
-//     //         int n = recv(client_sockfd, buffer, sizeof(buffer), 0);
-//     //         std::cout << "client: "<<n << std::endl;
-//     //         if (n <= 0) {
-//     //             //std::cout << "client closed the connection"<< std::endl;
-//     //             // Error or client closed the connection
-//     //             break;
-//     //         }
-//     //         if (send(server_sockfd, buffer, n, 0) < 0) {
-//     //             perror("ERROR sending data to server");
-//     //             break;
-//     //         }
-//     //     }
-//     //     else if (FD_ISSET(server_sockfd, &temp_set)) {
-//     //         char buffer[6];
-//     //         //std::cout << "inside server_sockfd"<< std::endl;
-//     //         int n = recv(server_sockfd, buffer, sizeof(buffer), 0);
-//     //         std::cout <<"server: "<< n << std::endl;
-//     //         if (n <= 0) {
-//     //             // Error or server closed the connection
-//     //             //std::cout << "server closed the connection"<< std::endl;
-//     //             break;
-//     //         }
-//     //         if (send(client_sockfd, buffer, n, 0) < 0) {
-//     //             perror("ERROR sending data to client");
-//     //             break;
-//     //         }
-//     //     }
-//     // }
-//     // close(server_sockfd);
-//     // close(client_sockfd);
-//     // std::stringstream str;
-//     // str << req.getID() << ": Tunnel closed"<< std::endl;
-//     // std::cout << str.str() << std::endl;
-// }
-
-
 
 int main(int argc, char ** argv) {
     if (argc != 2) {
